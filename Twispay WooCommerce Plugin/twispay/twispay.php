f270418e93fb965abda923240ec5cc2217ac1573
Ionut DINU <tunderzone@gmail.com>
2019-09-03 13:04:43 +0300

    Update twispay.php
    version bump

--- Twispay WooCommerce Plugin/twispay/twispay.php
+++ Twispay WooCommerce Plugin/twispay/twispay.php
@@ -3,7 +3,7 @@
  * Plugin Name: Twispay Credit Card Payments
  * Plugin URI: https://wordpress.org/plugins/twispay/
  * Description: Plugin for Twispay payment gateway.
- * Version: 1.0.4
+ * Version: 1.0.7
  * Author: twispay
  * Author URI: https://www.twispay.com
  * License: GPLv2
@@ -13,7 +13,7 @@
  * @package  Twispay
  * @category Core
  * @author   twispay
- * @version  1.0.4
+ * @version  1.0.7
  */
 
 // Exit if the file is accessed directly
